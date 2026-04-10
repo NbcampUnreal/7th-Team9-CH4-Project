1a180fea5faf45cc6576bf5d59a34449bd2f898a
yuna1005 <yunaj9910@gmail.com>
2026-04-10 10:15:14 +0900

    0410player/item

---
+++ Source/FarmsPopcorn/Item/FPItem.cpp
@@ -0,0 +1,71 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Item/FPItem.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Player/FPPlayerCharacter.h"
+
+AFPItem::AFPItem()
+{
+ 	
+	PrimaryActorTick.bCanEverTick = true;
+    bReplicates = true; // 멀티플레이 동기화
+
+    // 충돌 박스 설정
+    CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionBox"));
+    RootComponent = CollisionBox;
+    CollisionBox->SetBoxExtent(FVector(50.f, 50.f, 50.f));
+    CollisionBox->SetCollisionProfileName(TEXT("OverlapAllDynamic"));
+
+    // 메시 설정
+    ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMesh"));
+    ItemMesh->SetupAttachment(RootComponent);
+    ItemMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision); // 메시는 충돌 무시
+}
+
+
+void AFPItem::BeginPlay()
+{
+	Super::BeginPlay();
+	
+    // 서버에서만 충돌 감지 처리
+    if (HasAuthority())
+    {
+        CollisionBox->OnComponentBeginOverlap.AddDynamic(this, &AFPItem::OnOverlapBegin);
+    }
+}
+
+void AFPItem::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+    bool bFromSweep, const FHitResult& SweepResult)
+{
+    if (!HasAuthority()) return;
+
+    AFPPlayerCharacter* Player = Cast<AFPPlayerCharacter>(OtherActor);
+
+    // 플레이어가 살아있고, 이미 들고 있는 아이템이 없을 때만 획득
+    if (Player && Player->GetCurrentItem() == EItemType::None)
+    {
+        // 랜덤으로 아이템 결정 (None 제외 1~4번 중 하나)
+        // EItemType::Fan ~ EItemType::SweetPotato
+        int32 RandomItemIdx = FMath::RandRange(1, 4);
+        EItemType RandomItem = static_cast<EItemType>(RandomItemIdx);
+
+        // 플레이어에게 아이템 지급
+        Player->PickupItem(RandomItem);
+
+        // 상자 비활성화 (마리오카트처럼 먹으면 사라짐)
+        SetActorHiddenInGame(true);
+        SetActorEnableCollision(false);
+
+        // 일정 시간 후 재생성
+        GetWorldTimerManager().SetTimer(RespawnTimerHandle, this, &AFPItem::RespawnItem, RespawnTime, false);
+    }
+}
+
+void AFPItem::RespawnItem()
+{
+    SetActorHiddenInGame(false);
+    SetActorEnableCollision(true);
+}
---
+++ Source/FarmsPopcorn/Item/FPItem.h
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Player/FPPlayerCharacter.h" // EItemType 사용을 위해 필요
+#include "FPItem.generated.h"
+
+UCLASS()
+class FARMSPOPCORN_API AFPItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFPItem();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// 충돌 처리 함수
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+		bool bFromSweep, const FHitResult& SweepResult);
+
+	// 아이템 상자 재생성 함수
+	void RespawnItem();
+
+protected:
+	// 컴포넌트들
+	UPROPERTY(VisibleAnywhere, Category = "Item|Components")
+	TObjectPtr<class UBoxComponent> CollisionBox;
+
+	UPROPERTY(VisibleAnywhere, Category = "Item|Components")
+	TObjectPtr<UStaticMeshComponent> ItemMesh;
+
+	// 설정값
+	UPROPERTY(EditAnywhere, Category = "Item|Settings")
+	float RespawnTime = 5.0f; // 아이템 재생성 시간
+
+	FTimerHandle RespawnTimerHandle;
+
+};
