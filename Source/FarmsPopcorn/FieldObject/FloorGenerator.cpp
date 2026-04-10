ce0f9cfe8bef0937873d9798db8538e509babb8b
YoonjaeDong <yjdong114@naver.com>
2026-04-10 17:32:27 +0900

    레벨 바닥생성기 추가

---
+++ Source/FarmsPopcorn/FieldObject/FloorGenerator.cpp
@@ -0,0 +1,37 @@
+﻿#include "FloorGenerator.h"
+#include "Components/InstancedStaticMeshComponent.h"
+
+AFloorGenerator::AFloorGenerator()
+{
+    PrimaryActorTick.bCanEverTick = false;
+
+    InstancedMeshComp = CreateDefaultSubobject<UInstancedStaticMeshComponent>(TEXT("InstancedMeshComp"));
+    RootComponent = InstancedMeshComp;
+
+    Rows = 10;
+    Columns = 10;
+    BlockSize = 200.f; 
+}
+
+void AFloorGenerator::OnConstruction(const FTransform& Transform)
+{
+    Super::OnConstruction(Transform);
+
+    if (InstancedMeshComp)
+    {
+        InstancedMeshComp->ClearInstances();
+    }
+
+    for (int32 X = 0; X < Rows; ++X)
+    {
+        for (int32 Y = 0; Y < Columns; ++Y)
+        {
+            FVector InstanceLocation = FVector(X * BlockSize, Y * BlockSize, 0.f);
+
+            FTransform InstanceTransform;
+            InstanceTransform.SetLocation(InstanceLocation);
+
+            InstancedMeshComp->AddInstance(InstanceTransform);
+        }
+    }
+}
