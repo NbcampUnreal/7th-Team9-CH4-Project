a5dc1c0cc661369f112d46156bccaf4111c9b0f4
MoonIW <miws4747@gmail.com>
2026-04-13 20:30:11 +0900

    gameinstance추가,BP_Gamemode  수정

--- Source/FarmsPopcorn/Game/FPGameInstance.cpp
+++ Source/FarmsPopcorn/Game/FPGameInstance.cpp
@@ -2,3 +2,20 @@
 
 
 #include "FPGameInstance.h"
+
+#include "UI/FPUIManagerSubsystem.h"
+
+UFPGameInstance::UFPGameInstance()
+{
+	
+}
+
+void UFPGameInstance::Init()
+{
+	Super::Init();
+	UFPUIManagerSubsystem* Storage = GetSubsystem<UFPUIManagerSubsystem>();
+	if (Storage)
+	{
+		SaveNickName = Storage->SavedNickName;
+	}
+}
--- Source/FarmsPopcorn/Game/FPGameMode.cpp
+++ Source/FarmsPopcorn/Game/FPGameMode.cpp
@@ -1,5 +1,6 @@
 #include "Game/FPGameMode.h"
 #include "FPGameState.h"
+#include "FPGameInstance.h"
 #include "Player/FPPlayerState.h"
 
 
@@ -187,6 +188,7 @@ void AFPGameMode::ChangeTeam(AController* Player)
 	AFPPlayerState* FPPlayerState = Cast<AFPPlayerState>(Player->PlayerState);
 	if (!FPPlayerState) return;
 	UpdateTeamCounts();
+	UFPGameInstance* GI = GetGameInstance<UFPGameInstance>();
 	
 	if (FPPlayerState->TeamID == EFPTeamID::TeamRed)
 	{
@@ -196,6 +198,7 @@ void AFPGameMode::ChangeTeam(AController* Player)
 			return;
 		}
 		FPPlayerState->TeamID =EFPTeamID::TeamBlue;
+		GI->SaveTeamID = EFPTeamID::TeamBlue;
 		RedTeamCount--;  
 		BlueTeamCount++;  
 	}else if(FPPlayerState->TeamID == EFPTeamID::TeamBlue)
@@ -206,6 +209,7 @@ void AFPGameMode::ChangeTeam(AController* Player)
 			return;
 		}
 		FPPlayerState->TeamID =EFPTeamID::TeamRed;
+		GI->SaveTeamID = EFPTeamID::TeamRed;
 		RedTeamCount++;  
 		BlueTeamCount--; 
 	}
@@ -335,11 +339,15 @@ UClass* AFPGameMode::GetDefaultPawnClassForController_Implementation(AController
 
 void AFPGameMode::AddScoreToTeam(EFPTeamID InTeamID, int32 ScoreAmount)
 {
+	UFPGameInstance* GI = GetGameInstance<UFPGameInstance>();
+	
 	if (InTeamID == EFPTeamID::TeamRed)
 	{
 		RedTeamScore += ScoreAmount;
+		GI->SaveRedScore += ScoreAmount; 
 	}else if (InTeamID == EFPTeamID::TeamBlue)
 	{
 		BlueTeamScore += ScoreAmount;
+		GI->SaveBlueScore += ScoreAmount;
 	}
 }
