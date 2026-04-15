d389c4e66313d09710edb6ce3cb1ab613ddbd9ae
SSH1202 <tmdghks9812@gmail.com>
2026-04-15 15:11:31 +0900

    AI Enermy 추가

---
+++ Source/FarmsPopcorn/AI/BTTask_ChargePlayer.cpp
@@ -0,0 +1,64 @@
+﻿#include "BTTask_ChargePlayer.h"
+
+#include "AIController.h"
+#include "AI/FPSlimeCharacter.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+UBTTask_ChargePlayer::UBTTask_ChargePlayer()
+{
+	NodeName = TEXT("ChargePlayer");
+	bNotifyTick = true;
+}
+
+EBTNodeResult::Type UBTTask_ChargePlayer::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AFPSlimeCharacter* Slime = Cast<AFPSlimeCharacter>(OwnerComp.GetAIOwner()->GetPawn());
+	if (!Slime) return EBTNodeResult::Failed;
+	
+	ACharacter* Player = Cast<ACharacter>(OwnerComp.GetBlackboardComponent()->GetValueAsObject(PlayerKey.SelectedKeyName));
+	if (!Player) return EBTNodeResult::Failed;
+	
+	Slime->GetCharacterMovement()->MaxWalkSpeed = Slime->GetChargeSpeed();
+	
+	ChargedDirection = Player->GetActorLocation() - Slime->GetActorLocation();
+	ChargedDirection.Normalize();
+	ChargedDistance = 0.f;
+	
+	return EBTNodeResult::InProgress;
+}
+
+void UBTTask_ChargePlayer::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	AFPSlimeCharacter* Slime = Cast<AFPSlimeCharacter>(OwnerComp.GetAIOwner()->GetPawn());
+	if (!Slime)
+	{
+		FinishLatentTask(OwnerComp, EBTNodeResult::Failed);
+		return;
+	}
+	
+	FVector MoveAmount = ChargedDirection * Slime->GetChargeSpeed() * DeltaSeconds;
+	Slime->AddMovementInput(ChargedDirection, 1.0f);
+	ChargedDistance += MoveAmount.Size();
+	
+	TArray<AActor*> OverlappingActors;
+	Slime->GetOverlappingActors(OverlappingActors,ACharacter::StaticClass());
+	for (AActor* Actor : OverlappingActors)
+	{
+		ACharacter* Player = Cast<ACharacter>(Actor);
+		if (Player && Player != Slime)
+		{
+			Slime->ApplyKnockback(Player);
+			Slime->GetCharacterMovement()->MaxWalkSpeed = Slime->GetPatrolSpeed();
+			FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+			return;
+		}
+	}
+	
+	if (ChargedDistance >= MaxChargeDistance)
+	{
+		Slime->GetCharacterMovement()->MaxWalkSpeed = Slime->GetPatrolSpeed();
+		FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+	}
+}
---
+++ Source/FarmsPopcorn/AI/BTTask_FindPlayer.cpp
@@ -0,0 +1,37 @@
+﻿#include "AI/BTTask_FindPlayer.h"
+
+#include "AIController.h"
+#include "FPSlimeCharacter.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "GameFramework/Character.h"
+#include "Kismet/GameplayStatics.h"
+
+UBTTask_FindPlayer::UBTTask_FindPlayer()
+{
+	NodeName = TEXT("FindPlayer");
+}
+
+EBTNodeResult::Type UBTTask_FindPlayer::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AFPSlimeCharacter* Slime = Cast<AFPSlimeCharacter>(OwnerComp.GetAIOwner()->GetPawn());
+	if (!Slime) return EBTNodeResult::Failed;
+	
+	TArray<AActor*> Players;
+	UGameplayStatics::GetAllActorsOfClass(Slime, ACharacter::StaticClass(), Players);
+	
+	for (AActor* Actor : Players)
+	{
+		ACharacter* Player = Cast<ACharacter>(Actor);
+		if (!Player || Player == Slime) continue;
+		
+		float Distance = FVector::Dist(Slime->GetActorLocation(), Player->GetActorLocation());
+		
+		if (Distance <= Slime->GetDetectionRadius())
+		{
+			OwnerComp.GetBlackboardComponent()->SetValueAsObject(PlayerKey.SelectedKeyName, Player);
+			return EBTNodeResult::Succeeded;
+		}
+	}
+	OwnerComp.GetBlackboardComponent()->SetValueAsObject(PlayerKey.SelectedKeyName, nullptr);
+	return EBTNodeResult::Failed;
+}
