e4fd050db09bc1f58d154ccf6228dfe9eb133938
YoonjaeDong <yjdong114@naver.com>
2026-04-17 10:32:04 +0900

    랜덤블럭 레벨 생성기, 레벨 추가

---
+++ Source/FarmsPopcorn/FieldObject/FakeTile.cpp
@@ -0,0 +1,45 @@
+﻿// FakeTile.cpp
+
+#include "FakeTile.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/Character.h"
+
+AFakeTile::AFakeTile()
+{
+    PrimaryActorTick.bCanEverTick = false;
+
+    // 메쉬 설정
+    TileMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TileMesh"));
+    RootComponent = TileMesh;
+
+    // 트리거 박스 설정
+    TriggerBox = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerBox"));
+    TriggerBox->SetupAttachment(RootComponent);
+    TriggerBox->SetBoxExtent(FVector(90.f, 90.f, 20.f)); 
+    TriggerBox->SetRelativeLocation(FVector(0.f, 0.f, 100.f));
+
+    FallDelay = 0.3f; 
+}
+
+void AFakeTile::BeginPlay()
+{
+    Super::BeginPlay();
+
+    TriggerBox->OnComponentBeginOverlap.AddDynamic(this, &AFakeTile::OnPlayerStepped);
+}
+
+void AFakeTile::OnPlayerStepped(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+    if (OtherActor && OtherActor->IsA(ACharacter::StaticClass()))
+    {
+        GetWorldTimerManager().SetTimer(FallTimerHandle, this, &AFakeTile::DropTile, FallDelay, false);
+    }
+}
+
+void AFakeTile::DropTile()
+{
+    TileMesh->SetSimulatePhysics(true);
+
+    SetLifeSpan(3.0f);
+}
---
+++ Source/FarmsPopcorn/FieldObject/FakeTile.h
@@ -0,0 +1,40 @@
+﻿// FakeTile.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FakeTile.generated.h"
+
+UCLASS()
+class FARMSPOPCORN_API AFakeTile : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    AFakeTile();
+
+protected:
+    virtual void BeginPlay() override;
+
+    // 발판 외형
+    UPROPERTY(VisibleAnywhere, Category = "Components")
+    class UStaticMeshComponent* TileMesh;
+
+    // 밟았는지 감지할 센서
+    UPROPERTY(VisibleAnywhere, Category = "Components")
+    class UBoxComponent* TriggerBox;
+
+    // 밟고 나서 떨어지기까지의 지연 시간
+    UPROPERTY(EditAnywhere, Category = "Tile Settings")
+    float FallDelay;
+
+    // 타이머 핸들
+    FTimerHandle FallTimerHandle;
+
+    UFUNCTION()
+    void OnPlayerStepped(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+    // 발판을 떨어뜨리는 함수
+    void DropTile();
+};
