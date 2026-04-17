c671434a5e01537017b69f135ebbc4e0aa9a92b8
kkym323 <kkym32387@gmail.com>
2026-04-17 20:15:55 +0900

    spikeroller

---
+++ Source/FarmsPopcorn/FieldObject/SpikeRoller.cpp
@@ -0,0 +1,25 @@
+﻿#include "SpikeRoller.h"
+
+ASpikeRoller::ASpikeRoller()
+{
+    PrimaryActorTick.bCanEverTick = true;
+
+    RollerMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RollerMesh"));
+    RootComponent = RollerMesh;
+}
+
+void ASpikeRoller::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+void ASpikeRoller::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    float Direction = bReverseRotation ? -1.0f : 1.0f;
+
+    float RotationAmount = RotationSpeed * Direction * DeltaTime;
+
+    AddActorLocalRotation(FRotator(0.0f, RotationAmount, 0.0f));
+}
